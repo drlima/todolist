51d7afacda6d7ef996b0ca3918e87570d8faed2a
mindlessbrain <drodriguesdelima@gmail.com>
2018-02-14 18:01:18 -0200

    fully operational CRUD for todo app

--- todolist/todos/views.py
+++ todolist/todos/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Todo
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse('Hello World')
+    context = {
+    'name': 'Daniel',
+    'todos': Todo.objects.all()[:10]
+    }
+    return render(request, 'index.html', context)
+
+
+def details(request, id):
+    context = {
+        'todo' : Todo.objects.get(id=id)
+    }
+    return render(request, 'details.html', context)
